4e98a84a350a46f1208a48b736d4267287212a6d
Darc <kadenrobert2004@gmail.com>
2026-08-24 13:43:58 -0500

    start

---
+++ Button_Practice.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#define NUM_BUTTONS 4
+const uint BUTTON_PINS[NUM_BUTTONS] = {14, 15, 16, 17};
+
+int main()
+{
+    stdio_init_all();
+    for(int i =  0; i < NUM_BUTTONS; i++){
+    gpio_init(BUTTON_PINS[i]);
+    gpio_set_dir(BUTTON_PINS[i], GPIO_IN);
+    gpio_pull_up(BUTTON_PINS[i]);
+    }
+
+    bool last_state[NUM_BUTTONS];
+    for(int i = 0; i< NUM_BUTTONS; i++){
+        last_state[i] = true;
+    }
+
+    while(true){
+        for (int i = 0; i < NUM_BUTTONS; i++){
+            bool current_state = gpio_get(BUTTON_PINS[i]);
+
+            if  (last_state[i] && !current_state){
+                printf("button %d pressed\n", BUTTON_PINS[i]);
+            }
+        }
+        sleep_ms(250);
+    }
+
+}
