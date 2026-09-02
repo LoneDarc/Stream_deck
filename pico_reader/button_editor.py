bc66e148f0aad6b5c61bce9082b46758fdbcc5a8
LoneDarc <kadenrobert2004@gmail.com>
2026-09-02 08:48:16 -0500

    continue working on json formatting and display

--- pico_reader/button_editor.py
+++ pico_reader/button_editor.py
@@ -2,12 +2,61 @@ import json
 import os
 
 def display_profile(profile):
-    print(profile)
+    print("profile: " + profile["profile_0"]["profile_name"])
+    print("--------------------")
+    print("Buttons: ")
+
+    for x in range(0,8):
+        print(x , ": Label:     " + profile["profile_0"]["Buttons"][str(x)]["label"] )
+        print("    task_type: " + profile["profile_0"]["Buttons"][str(x)]["task_type"])
+        print("    task:      " + profile["profile_0"]["Buttons"][str(x)]["task"])
 
 base_setting = {
-    "Button": 0,
-    "Task_type": "None",
-    "Task": "None"}
+    "profile_0": { "profile_name" : "default",
+        "Buttons": {
+            0 : {
+                "label" : "",
+                "task_type" : "",
+                "task" : ""
+            },
+            1 : {
+                "label" : "",
+                "task_type" : "",
+                "task" : ""
+            },
+            2 : {
+                "label" : "",
+                "task_type" : "",
+                "task" : ""
+            },
+            3 : {
+                "label" : "",
+                "task_type" : "",
+                "task" : ""
+            },
+            4 : {
+                "label" : "",
+                "task_type" : "",
+                "task" : ""
+            },
+            5 : {
+                "label" : "",
+                "task_type" : "",
+                "task" : ""
+            },
+            6 : {
+                "label" : "",
+                "task_type" : "",
+                "task" : ""
+            },
+            7 : {
+                "label" : "",
+                "task_type" : "",
+                "task" : ""
+            },
+        }
+    }
+}
 
 
 print("Welcome to the stream deck editor\n\n")
